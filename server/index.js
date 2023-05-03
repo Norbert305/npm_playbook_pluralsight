3576b73f8ff0fe8f0b9793701db7a73c1b2e023e
Norbert305 <sealsnorbert@yahoo.com>
2023-05-03 14:20:47 -0400

    npm setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+
+//use command npm start in the terminal :)
+//cd into the server directory
+
+function helloWorld (yolo) {
+    console.log("Hello World");
+}
+
+helloWorld()
+
+
+const subtract =(a,b)=>{
+    return a - b;
+}
+
+console.log(subtract(20,10));
---
+++ server/test.js
@@ -0,0 +1,8 @@
+
+//use command npm test in the terminal :)
+//cd into the server directory
+function add (a,b) {
+    return a + b;
+}
+
+console.log(add(5,5));
